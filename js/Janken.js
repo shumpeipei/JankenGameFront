11bb287f355c6b1d015067606452c87b446ddf69
shm <39214521+shumpeipei@users.noreply.github.com>
2021-03-28 21:10:30 +0900

    update0328

--- js/Janken.js
+++ js/Janken.js
@@ -1,42 +1,47 @@
 
-var cpHand ;
-var playerHand;
-var judge;
-
-const JUDGE = new Array("あなた","コンピューター","引き分け"); 
-const JANKEN_HANDS = new Array("グー","チョキ","パー");
-
 function main(hand){
-    
+
+    let cpHand ;
+    let playerHand;
+    let result;
+
+    const JANKEN_HANDS = ['グー','チョキ','パー'];
+    const JUDGE = ['勝ち','負け','引き分け']; 
+
     playerHand = hand;
     //CPの手をランダム生成
     cpHand = Math.floor(Math.random() * 3);
 
     //勝敗判定
-    judge = Judge(playerHand,cpHand);
+    result = judge(playerHand,cpHand);
 
-    alert("勝敗は"+ JUDGE[judge] +"。\nあなたが出した手は" + JANKEN_HANDS[playerHand] +"です\n相手が出した手は" + JANKEN_HANDS[cpHand] +"です");
+    if(result === 2){
+        alert(`勝敗は引き分け\nあなたが出した手は${JANKEN_HANDS[playerHand]}\nコンピューターが出した手は${JANKEN_HANDS[cpHand]}`);
+    }else{
+        alert(`あなたは${JUDGE[result]} \nあなたが出した手は${JANKEN_HANDS[playerHand]}\nコンピューターが出した手は${JANKEN_HANDS[cpHand]}`);
+    }
+
     
 }
 
-function Judge(playerHand,cpHand){
+function judge(playerHand,cpHand){
 
     const JUDGE_PLAYER = 0;
     const JUDGE_COMPUTER = 1;
     const JUDGE_DRAW = 2;
     
     //勝敗判定
-    if(playerHand == 0 && cpHand == 1){
+    if(playerHand === 0 && cpHand === 1){
         return JUDGE_PLAYER;
-    }else if(playerHand == 0 && cpHand == 2){
+    }else if(playerHand === 0 && cpHand === 2){
         return JUDGE_COMPUTER;
-    }else if(playerHand == 1 && cpHand == 0){
+    }else if(playerHand === 1 && cpHand === 0){
         return JUDGE_COMPUTER;
-    }else if(playerHand == 1 && cpHand == 2){
+    }else if(playerHand === 1 && cpHand === 2){
         return JUDGE_PLAYER;
-    }else if(playerHand == 2 && cpHand == 0){
+    }else if(playerHand === 2 && cpHand === 0){
         return JUDGE_PLAYER;
-    }else if(playerHand == 2 && cpHand == 1){
+    }else if(playerHand === 2 && cpHand === 1){
         return JUDGE_COMPUTER;
     }else{
         return JUDGE_DRAW;
@@ -44,10 +49,4 @@ function Judge(playerHand,cpHand){
 
 
 }
-
-
-
-
-
-
 
